3c4fbf8772bc1e05b2f396300bb4186033944fc2
Michał Rudzki <michal@mailx.com.pl>
2023-05-28 20:14:01 +0200

    defining zakup function

--- app.py
+++ app.py
@@ -6,29 +6,53 @@ import json
 import jinja2
 
 from modules.myglobal import FILEDB
-from modules.files_operation import VaultDB
+from modules.files_operation import VaultDB, Warehouse
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     title = "Witaj"
-    db = VaultDB(FILEDB)
-    file_db = db.openFile()
+    #db = VaultDB(FILEDB)
+    #file_db = db.openFile()
+    db = Warehouse(FILEDB)
+    file_db = db.openFileToRead()
+    
     saldo = sum([int(s) for s in file_db['saldo'].keys()])
+    magazyn = [(k, v) for k,v in file_db['magazyn'].items()]
 
     operation = request.form.get('accountent')
     name = request.form.get('name')
     quantity = request.form.get('quantity')
     prize = request.form.get('prize')
     
-    return render_template('index.html', title=title, db=db, saldo=saldo)
+    return render_template('index.html', title=title, db=db, file_db=file_db, saldo=saldo, magazyn=magazyn)
+
+@app.route('/zakup/', methods=['GET', 'POST'])
+def zakup():
+    db = Warehouse(FILEDB)
+    file_db = db.openFileToRead()
+    
+    item = request.args.get('item')
+    quantity = request.args.get('quantity')
+    
+    if not item:
+        return {'sukces': False, 'msg': 'Nie podano nazwy dla przedmiotu'}
+    
+    try:
+        quantity = float(quantity)
+    except ValueError:
+        return {'sukces': False, 'msg': 'Liczba sztuk musi być podana jako rzeczywista z kropką'}
+    
+    db.addItem(file_db, item, quantity)
+    
+    return {'sukcess': True, 'msg:': f'Item: {item}, Quantity: {quantity}'}
 
 @app.route('/history/')
 def history():
     title = 'History page'
-    db = VaultDB(FILEDB)
-    file_db = db.openFile()
+    db = Warehouse(FILEDB)
+    file_db = db.openFileToRead()
     history = file_db['historia']
     
     return render_template('history.html', title=title, history=history)
