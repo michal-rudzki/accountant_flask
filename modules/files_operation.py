f5e8b3ae1dd83921dfbee46213bcc5a45f07c19c
Michał Rudzki <michal@mailx.com.pl>
2023-05-28 20:14:56 +0200

    configuring Warehouse class

--- modules/files_operation.py
+++ modules/files_operation.py
@@ -1,12 +1,37 @@
 import json
+from modules.myglobal import FILEDB
 
 class VaultDB:
     
     def __init__(self, *args, **kwargs):
-        self.db_file = args[0]
+        self.db_file = args[1]
     
-    def openFile(self):
+    def openFileToRead(self):
         with open(self.db_file, mode='r', encoding='utf-8') as filedb:
             db = json.load(filedb)
         return db
+    
+    def openFileToSave(self, db):
+        with open(self.db_file, mode='w', encoding='utf-8') as filedb:
+            json.dump(db, filedb)
+
+class Warehouse(VaultDB):
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(self, *args, **kwargs)
         
+    def loadFile(self):
+        pass
+        
+    def addItem(self, file_db, item, quantity):
+        file_db['magazyn'].update({item: quantity})
+        self.openFileToSave(file_db)
+    
+    def checkItem(self, item):
+        pass
+    
+    def checkQuantity(self, quantity):
+        pass
+    
+    def removeItem(self, item, quantity):
+        pass
